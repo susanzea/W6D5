a8d44b5efd189796f92885749caffa8bc4c02d1a
chayacohen <chayac100@gmail.com>
2021-11-12 15:20:12 -0500

    working on cats_controller

---
+++ cats_99/app/controllers/cats_controller.rb
@@ -0,0 +1,48 @@
+class CatsController < ApplicationController
+
+
+    def index 
+        @cats = Cat.all 
+        render :index
+    end 
+
+    def new
+        @cat = Cat.new 
+        render :new
+    end 
+
+    def edit 
+        @cat = Cat.find(params[:id])
+        render :edit 
+    end 
+
+    def show 
+        @cat = Cat.find(params[:id])
+        render :show
+    end 
+
+    def create 
+        @cat = Cat.new(cats_params)
+        if @cat.save
+            redirect_to cat_url
+        else 
+            render json: @cat.errors.full_messages, status: 422
+        end 
+    end 
+
+    def destroy 
+        @cat = Cat.find(params[:id])
+        @cat.destroy 
+        redirect_to cats_url
+    end 
+
+    def cats_params
+        params.require(:cat).permit(:birthdate, :color, :name, :sex, :description)
+    end 
+
+
+
+
+
+
+end 
--- cats_99/app/models/cat.rb
+++ cats_99/app/models/cat.rb
@@ -3,9 +3,10 @@ require 'action_view'
 class Cat < ApplicationRecord
     include ActionView::Helpers::DateHelper
     COLORS = %w(white black gray brown)
+    SEX = %w(M F)
     validates :birthdate, :color, :name, :sex, :description, presence: true
     validates :color, inclusion: COLORS
-    # validates :color, :inclusion => { in: COLORS }
+    validates :sex, inclusion: SEX
 
     def age 
         time_ago_in_words(birthdate)
